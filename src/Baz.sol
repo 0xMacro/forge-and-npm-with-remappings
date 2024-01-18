693ce265f8aa639a5602258b5e5e886f292f6ec4
Gilbert <gilbertbgarza@gmail.com>
2024-01-18 14:00:13 -0600

    Sample repo

---
+++ src/Baz.sol
@@ -0,0 +1,6 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import "@openzeppelin-upgradeable/contracts/security/ReentrancyGuardUpgradeable.sol";
+
+contract Baz is ReentrancyGuardUpgradeable {}
